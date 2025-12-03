8b69f9d077fb8ab2ba4df672ed9a42a80fba587f
mattpetts <mattpetts23@gmail.com>
2025-12-03 16:41:01 +0000

    tidy up files and install dependancies

--- blocks/read-more/src/index.js
+++ blocks/read-more/src/index.js
@@ -1,10 +1,5 @@
 import { registerBlockType } from '@wordpress/blocks';
-
 import './style.scss';
-
-/**
- * Internal dependencies
- */
 import Edit from './edit';
 import metadata from '../block.json';
 
